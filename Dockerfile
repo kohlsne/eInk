5d92f1034cb8cc18766211665dc175bab2f2d713
Nicholas Kohls <kohlsn@gmail.com>
2022-09-02 09:40:10 -0700

    Mounted www/pics folder

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 ARG BUILD_FROM
 FROM $BUILD_FROM
 
+
+WORKDIR /config/www/pics
 # Copy data for add-on
 COPY run.sh /
-RUN chmod a+x /run.sh
+
+RUN \
+  apk add --no-cache \
+    imagemagick
+
 
 CMD [ "/run.sh" ]
--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/with-contenv bashio
 
 echo "Hello world!"
-
+magick -version
+pwd
+ls -l
+#Grab A random photo
+#sftp with key?
+#/config/www/pics/nature-images.jpg 
+magick nature-images.jpg pic.png 
+echo "magic complete"
+ls -l
+#sftp {user}@{host}:{remoteFileName} {localFileName}
+#sftp root@172.31.0.2:/config/www/pics/nature-images.jpg /photo/.
